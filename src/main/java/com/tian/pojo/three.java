0dbccae8353d0e4af234f5f236f59d9a3022b990
airpig1999 <1319266933@qq.com>
2022-01-20 19:33:52 +0800

    second test

--- src/main/java/com/tian/pojo/three.java
+++
@@ -1,8 +0,0 @@
-package com.tian.pojo;
-
-/**
- * @author airpig1999
- * @date 2022/1/20 - 14:52
- */
-public class three {
-}
--- src/main/java/com/tian/pojo/two.java
+++
@@ -1,8 +0,0 @@
-package com.tian.pojo;
-
-/**
- * @author airpig1999
- * @date 2022/1/20 - 14:13
- */
-public class two {
-}
